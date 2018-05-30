593eb8c2675988dee86f98124a6f2af236ac1d44
Danny Wahl <danny@iyware.com>
2018-05-30 09:55:37 -0600

    remove app object in workflow example

--- workflow.js
+++ workflow.js
@@ -4,8 +4,8 @@ exports.process = async function process(description, role, title, file, url, ch
       await chromeless
         .goto(url)
         .screenshot({filePath:file})
-        .type(config.app.users[role]["username"], '#username')
-        .type(config.app.users[role]["password"], '#password')
+        .type(config.users[role]["username"], '#username')
+        .type(config.users[role]["password"], '#password')
         .click('#login')
       break;
     default:
